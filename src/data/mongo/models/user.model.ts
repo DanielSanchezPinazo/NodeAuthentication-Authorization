17cdc56f1c32d834e41f3e190102187717bfd734
Daniel Sanchez <danielsanchezpinazo@gmail.com>
2024-01-24 21:47:24 +0100

    UserModel added

---
+++ src/data/mongo/models/user.model.ts
@@ -0,0 +1,34 @@
+
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [ true, "Name is required."],// es requerido y añadimos un mensaje de error
+    },
+    email: {
+        type: String,
+        required: [ true, "Email is required."],
+        unique: true, // esto es para indicar que no se puede repetir
+    },
+    password: {
+        type: String,
+        required: [ true, "Password is required."],// es requerido y añadimos un mensaje de error
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: String,
+        default: ["USER_ROLE"],
+        enum: [ "ADMIN_ROLE", "USER_ROLE" ],
+    }
+});
+
+export const UserModel = mongoose.model( "User", userSchema );
+
+
+
+
